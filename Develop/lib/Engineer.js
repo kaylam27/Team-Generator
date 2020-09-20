04e343616f617171bd209f4908a5959994136ff1
Kayla <kaylajmadore@gmail.com>
2020-09-20 16:16:54 -0700

    inital commit, engineer and employee files complete

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,18 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, username) {
+        super(name, id, email);
+        this.username = username;
+    };
+    getRole() {
+        return "Engineer";
+    };
+    getUsername() {
+        return this.username;
+    };
+
+};
+
+module.exports = Engineer
